baa861ad3d5b46163e3b2d61f47fe20533c8c84c
Steven Roland <steven@mojave.io>
2016-05-28 21:27:50 -0400

    Basic server
    - Created a basic server to test.

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 'use strict';
+// ------ Require packages
+var express = require('express');
+
+// ------ Configure application
+var app     = express();
+var port    = process.env.PORT || 3000;
+
+// ------ Build routes
+app.get('/', function(req, res) {
+  res.send("Hello, beautiful!");
+});
+
+// ------ Serve
+app.listen(port, function() {
+  console.log("Running on port " + port);
+});
