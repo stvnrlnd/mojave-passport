8bd932d3749443a99bf760115e96953ec7b2145b
Steven Roland <steven@mojave.io>
2016-05-28 23:55:25 -0400

    Account basics
    - Created login, register, and settings pages.
    - Modified the index.html file so that links are always displayed.

--- dev/app/core.js
+++ dev/app/core.js
@@ -6,6 +6,18 @@
           .state('home', {
             url: '/',
             templateUrl: 'templates/public/home.html'
+          })
+          .state('register', {
+            url: '/register',
+            templateUrl: 'templates/accounts/register.html'
+          })
+          .state('login', {
+            url: '/login',
+            templateUrl: 'templates/accounts/login.html'
+          })
+          .state('settings', {
+            url: '/settings',
+            templateUrl: 'templates/accounts/settings.html'
           });
         $urlRouterProvider
           .otherwise('/');
